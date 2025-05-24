263087847c9919c8d0a2c44bbc4c5e4fb46d5387
FADI <fahadkiyani28@gmail.com>
2025-05-25 00:44:17 +0500

    animation-reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from '/src/components/Header.jsx'
-import Footer from '/src/components/Footer.jsx'
-import ScrollToTopButton from '/src/components/ScrollToTopButton.jsx'
-import Loader from './components/Loader'
-import { useState, useEffect } from 'react'
-import Aos from 'aos'
-import 'aos/dist/aos.css';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "/src/components/Header.jsx";
+import Footer from "/src/components/Footer.jsx";
+import ScrollToTopButton from "/src/components/ScrollToTopButton.jsx";
+import Loader from "./components/Loader";
+import { useState, useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Layout() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
+    const timer = setTimeout(
+      () => {
+        setLoading(false);
 
-      Aos.init({
-        duration: 1000, // Animation time
-        once: true,     // Animation only once
-      });
-    }, 3000,[]); 
-    
+        Aos.init({
+          duration: 1000, // Animation time
+          once: false, // Animation only once
+        });
+      },
+      3000,
+      []
+    );
+
     // 2 seconds loader
     // useEffect(() => {
     //   Aos.init({
@@ -33,17 +37,12 @@ export default function Layout() {
 
   if (loading) return <Loader />;
 
-  
- 
-  
   return (
     <>
-
-
-    <Header/>
-    <Outlet/>
-    <Footer/>
-    <ScrollToTopButton/>
+      <Header />
+      <Outlet />
+      <Footer />
+      <ScrollToTopButton />
     </>
-  )
+  );
 }
